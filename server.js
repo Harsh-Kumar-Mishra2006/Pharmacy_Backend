7d1ba568878e163e8fac830b91d8e31ddc9b969d
Harsh-Kumar-Mishra <mishrakumarharsh987@gmail.com>
2026-09-21 20:17:46 +0530

    add CORS origins

--- server.js
+++ server.js
@@ -11,8 +11,16 @@ dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 5000;
 
+const corsOptions = {
+  origin: [
+    'http://localhost:5173',
+    'https://pharmacy-frontend-lp3h.onrender.com'
+  ],
+  credentials: true
+};
+
 // Middleware
-app.use(cors());
+app.use(cors(corsOptions));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
